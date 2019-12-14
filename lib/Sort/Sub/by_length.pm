1a92a814b61c3c4bd30e58c40601c9c7e59fbd00
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-12-14 13:38:28 +0700

    Add sorter: by_length

---
+++ lib/Sort/Sub/by_length.pm
@@ -0,0 +1,36 @@
+package Sort::Sub::by_length;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+
+sub gen_sorter {
+    my ($is_reverse, $is_ci) = @_;
+
+    sub {
+        no strict 'refs';
+
+        my $caller = caller();
+        my $a = @_ ? $_[0] : ${"$caller\::a"};
+        my $b = @_ ? $_[1] : ${"$caller\::b"};
+
+        my $cmp = length($a) <=> length($b);
+        $is_reverse ? -1*$cmp : $cmp;
+    };
+}
+
+1;
+# ABSTRACT: Sort by length of string
+
+=for Pod::Coverage ^(gen_sorter)$
+
+=head1 DESCRIPTION
+
+This is equivalent to Perl's:
+
+ sub { length($a) <=> length($b) }
